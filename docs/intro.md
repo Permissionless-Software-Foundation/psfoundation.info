26095547d43854465c0fdef02f1c030b9ed09029
Chris Troutner <chris.troutner@gmail.com>
2022-05-19 16:04:25 -0700

    Forked from p2wdb.com

---
+++ docs/intro.md
@@ -0,0 +1,12 @@
+---
+sidebar_position: 1
+---
+
+# Introduction
+
+The Pay-to-Write Database (P2WDB) is an open, decentralized, peer-to-peer (p2p) database. Like a blockchain, the database has many redundant copies spread across the globe. They sync with one another using consensus rules. This makes the database very difficult to censor, and a great place to store sensitive data that may be subject to censorship. Unlike a blockchain, the database is optimized for storing data. Each entry can store up to 10 KB of data, and costs 0.01 [PSF tokens](https://psfoundation.cash).
+
+There are a handful of repositories that make up the P2WDB ecosystem:
+
+- [p2wdb](https://www.npmjs.com/package/p2wdb) is a JavaScript library that can be used by web apps to read from and write to the P2WDB.
+- [ipfs-p2wdb-service](https://github.com/Permissionless-Software-Foundation/ipfs-p2wdb-service) is a node.js JavaScript app that will install and run an instance of the P2WDB.
---
+++ docusaurus.config.js
@@ -0,0 +1,116 @@
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github')
+const darkCodeTheme = require('prism-react-renderer/themes/dracula')
+const filecoin = require('./static/filecoin.json')
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'P2WDB',
+  tagline: 'Pay-to-Write Database',
+  url: 'https://permissionless-software-foundation.github.io',
+  baseUrl: '/p2wdb.com/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/logo.png',
+  organizationName: 'permissionless-software-foundation', // Usually your GitHub org/user name.
+  projectName: 'p2wdb.com', // Usually your repo name.
+  trailingSlash: false,
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl: 'https://github.com/Permissionless-Software-Foundation/p2wdb.com/tree/main'
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/Permissionless-Software-Foundation/p2wdb.com/tree/main'
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css')
+        }
+      })
+    ]
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'P2WDB',
+        logo: {
+          alt: 'P2WDB',
+          src: 'img/logo.png'
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: 'Docs'
+          },
+          // { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: 'https://github.com/Permissionless-Software-Foundation/p2wdb.com',
+            label: 'GitHub',
+            position: 'right'
+          }
+        ]
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'CashStack',
+                to: '/docs/intro'
+              }
+            ]
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Telegram',
+                href: 'https://t.me/bch_js_toolkit'
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/PSF_DAO'
+              }
+            ]
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Filecoin',
+                to: `https://${filecoin.cid}.ipfs.dweb.link`
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/Permissionless-Software-Foundation/p2wdb.com'
+              }
+            ]
+          }
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} released under MIT License.`
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme
+      }
+    })
+}
+
+module.exports = config
